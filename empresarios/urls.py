b434734a93e7d369a24b500412d73ee830b352ca
barba-branca <kaueneves53@gmail.com>
2024-10-29 15:34:10 -0300

    commit novo

--- empresarios/urls.py
+++ empresarios/urls.py
@@ -7,4 +7,4 @@ urlpatterns = [
     path('cadastrar_empresa/', views.cadastrar_empresa, name="cadastrar_empresa"),
     path('listar_empresas/', views.listar_empresas, name="listar_empresas"),
     path('empresa/<int:id>', views.empresa, name="empresa")
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
