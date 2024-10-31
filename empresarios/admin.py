9310ffefd9eb5ca2720cfbbeeadb866e2ace1ad3
barba-branca <kaueneves53@gmail.com>
2024-10-31 02:39:29 -0300

    novo commit
    ta acontecendo um bug no urls.py
    path('add_doc/<int:id>', views.add_doc, name="add_doc"),

--- empresarios/admin.py
+++ empresarios/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Empresas
+from .models import Empresas, Documento
 # Register your models here.
 
 admin.site.register(Empresas)
+admin.site.register(Documento)
